f2c535bb86600da48ea94fac5e7de93778a0efd0
Cătălina-Lidia Drăgan <catalina.dragan13@yahoo.com>
2021-08-04 15:13:20 +0300

    toDo-list-generator

---
+++ script.js
@@ -0,0 +1,29 @@
+let input = document.querySelector("#add");
+let btn = document.querySelector("#btn");
+let list =  document.querySelector("#list");
+let el = document.getElementsByTagName("li");
+
+
+btn.addEventListener('click',() => {
+    let text = input.value;
+    if(text === ""){
+        alert("you must write something");
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = text;
+        list.insertBefore(li, list.childNodes[0]);
+        input.value = '';
+    }
+    
+});
+
+
+list.addEventListener('click', e =>{
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle('checked');
+    }
+
+});
+
+
+
